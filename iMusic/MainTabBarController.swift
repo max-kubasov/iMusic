de3dbbe741064fcea272342f5efa5d32b40a719e
max-kubasov <kubasov87@gmail.com>
2024-01-25 13:34:04 +0200

    Setup MainTabBarController, add searchVC and LibraryVC

---
+++ iMusic/MainTabBarController.swift
@@ -0,0 +1,38 @@
+//
+//  MainTabBarController.swift
+//  iMusic
+//
+//  Created by Max on 25.01.2024.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //tabBar.barTintColor = .lightGray
+        view.backgroundColor = .cyan
+        
+        tabBar.tintColor = .red
+        
+        
+        viewControllers = [
+            generateViewController(rootViewController: SearchViewController(), image: "search", title: "Search"),
+            generateViewController(rootViewController: ViewController(), image: "library", title: "Library")
+        ]
+    }
+    
+    
+    private func generateViewController(rootViewController: UIViewController, image: String, title: String) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.image = UIImage(named: image)
+        navigationVC.tabBarItem.title = title
+        rootViewController.navigationItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        return navigationVC
+        
+    }
+    
+}
