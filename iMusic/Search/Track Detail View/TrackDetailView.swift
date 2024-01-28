23ff521c0a2600fca10512c80d3a020b1d9d1bcd
max-kubasov <kubasov87@gmail.com>
2024-01-28 17:22:45 +0200

    Add play track with AVPlayer

--- iMusic/Search/Track Detail View/TrackDetailView.swift
+++ iMusic/Search/Track Detail View/TrackDetailView.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import SDWebImage
+import AVKit
 
 class TrackDetailView: UIView {
     
@@ -14,23 +16,43 @@ class TrackDetailView: UIView {
     @IBOutlet weak var currentTimeSlider: UISlider!
     @IBOutlet weak var currentTimeLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
-    
     @IBOutlet weak var trackTitleLabel: UILabel!
-    
     @IBOutlet weak var authorTitleLabel: UILabel!
-    
-    
     @IBOutlet weak var playPauseButton: UIButton!
-    
     @IBOutlet weak var volumeSlider: UISlider!
     
-    
+    let player: AVPlayer = {
+        let avPlayer = AVPlayer()
+        avPlayer.automaticallyWaitsToMinimizeStalling = false
+        return avPlayer
+    }()
     
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         trackImageView.backgroundColor = .cyan
+    }
+    
+    func set(viewModel: SearchViewModel.Cell) {
+        trackTitleLabel.text = viewModel.trackName
+        authorTitleLabel.text = viewModel.artistName
+        playTrack(previewUrl: viewModel.previewUrl)
+        
+        let string600 = viewModel.iconUrlString?.replacingOccurrences(of: "100x100", with: "600x600")
+        guard let url = URL(string: string600 ?? "") else { return }
+        
+        trackImageView.sd_setImage(with: url)
+        
+    }
+    
+    private func playTrack(previewUrl: String?) {
+        print("Try on track from link: \(previewUrl ?? "No track")")
+        
+        guard let url = URL(string: previewUrl ?? "") else { return }
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
     }
     
     
@@ -51,6 +73,14 @@ class TrackDetailView: UIView {
     }
     
     @IBAction func playPauseAction(_ sender: Any) {
+        if player.timeControlStatus == .paused {
+            player.play()
+            playPauseButton.setImage(UIImage(named: "pause"), for: .normal)
+        } else {
+            player.pause()
+            playPauseButton.setImage(UIImage(named: "play"), for: .normal)
+        }
+        
     }
     
 }
